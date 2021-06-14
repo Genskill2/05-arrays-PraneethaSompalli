49469d1f5d7fe556b0813a41f6651d9131ab02be
PraneethaSompalli <85060536+PraneethaSompalli@users.noreply.github.com>
2021-06-14 10:18:14 +0530

    min soluns.c

--- solns.c
+++ solns.c
@@ -3,10 +3,22 @@
 
 int max(int a[], int n){
   int max = a[0];
-  for(i=1;i<n ; i++){
+  int i;
+  for( i=1;i<n ; i++){
     if(a[i]>max){
       max = a[i];
     }
   }
   return max;
+}
+
+int min( int a[] , int n ) {
+  int min = a[0];
+  int i;
+  for(i=1;i<n;i++){
+    if(a[i]<min){
+      min = a[i];
+    }
+  }
+  return min;
 }
