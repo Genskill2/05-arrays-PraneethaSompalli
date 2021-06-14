4e8f7c6f2fef377f18ebfd667c2c3421f713d41a
PraneethaSompalli <85060536+PraneethaSompalli@users.noreply.github.com>
2021-06-14 10:24:54 +0530

    avg sol.c

--- solns.c
+++ solns.c
@@ -22,3 +22,14 @@ int min( int a[] , int n ) {
   }
   return min;
 }
+
+float average( int a[], int n){
+  int i ;
+  float sum= 0;
+  for(i=0;i<n; i++){
+    sum= sum+ a[i];
+  }
+  return sum/n;
+}
+  
+    
