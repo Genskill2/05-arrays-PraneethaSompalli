f41dc763cb66152b5e99db9aa942867105fd2b42
PraneethaSompalli <85060536+PraneethaSompalli@users.noreply.github.com>
2021-06-14 10:13:11 +0530

    max.solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int a[], int n){
+  int max = a[0];
+  for(i=1;i<n ; i++){
+    if(a[i]>max){
+      max = a[i];
+    }
+  }
+  return max;
+}
